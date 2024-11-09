831aa96b7227b1e519fdef5bd2733375b3592295
faizm10 <faizmustansar10@gmail.com>
2024-11-08 22:37:30 -0500

    init

---
+++ frontend/src/pages/_app.js
@@ -0,0 +1,18 @@
+// import Navbar from ''
+import * as React from "react";
+
+// 1. import `NextUIProvider` component
+import { NextUIProvider } from "@nextui-org/react";
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <NextUIProvider>
+        {/* <Navbar /> */}
+        <Component {...pageProps} />
+      </NextUIProvider>
+    </>
+  );
+}
+
+export default MyApp;
